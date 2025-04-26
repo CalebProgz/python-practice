9fffcb11bf556009b5a7edac550da958cd04748d
Caleb Munene <munene.caleb@strathmore.edu>
2025-04-26 21:31:02 +0300

    Update.

--- Factorials(Recursive).py
+++ Factorials(Recursive).py
@@ -6,9 +6,11 @@ def factorial(x):
         return x * factorial(x-1)
     
 def main():
+    # User input for factorial calculation
     n2 = int(input("Enter a value to find the factorials: "))
     result = factorial(n2)
 
+    # Print the result
     print(f"The factorial of {n2} is {result}")
 
 if __name__ == "__main__":
